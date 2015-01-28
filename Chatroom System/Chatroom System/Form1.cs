97c60d1c89ce835e5a25985f39ed79fb0e6f2fb2
norberte <utherpendragon17@yahoo.com>
2015-01-27 21:24:31 -0800

    Write Data to File changes
    I worked on the data structure methods. Norbert

--- Chatroom System/Chatroom System/Form1.cs
+++ Chatroom System/Chatroom System/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Chatroom_System
 {
@@ -16,5 +17,38 @@ namespace Chatroom_System
         {
             InitializeComponent();
         }
-    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            createfile();
+        }
+
+        public void createfile()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text File|*.txt";
+            sfd.FileName = "My Messages";
+            sfd.Title = "Save the conversation";
+
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                writeStringToFile(sfd.FileName, "Hello World !" + Environment.NewLine);
+                writeStringToFile(sfd.FileName, "Hello World !" + Environment.NewLine);
+                writeStringToFile(sfd.FileName, "Hello World !" + Environment.NewLine);
+            }
+        }
+
+        public void writeStringToFile(String filePath,String content)
+        {
+            // File.WriteAllText(path, s);
+            File.AppendAllText(filePath, content);
+        }
+
+        public String readStringFromFile(String filePath)
+        {
+            StreamReader sr = new StreamReader(File.OpenRead(filePath));
+            return sr.ReadToEnd();
+        }
+        
+        }
 }
